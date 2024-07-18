1964626925bbda7695e3c9cc2789a56d11124c71
Israel Moreno <yxrael@gmail.com>
2024-07-18 18:06:08 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { EnterNumberScreen } from './src/screens/EnterNumberScreen';
+import OpponentGuessScreen from './src/screens/OpponentGuessScreen';
+import { GameOverScreen } from './src/screens/GameOverScreen';
+
+
 
 export default function App() {
+
+  // ['EnterNumber', 'GameOver','OpponentGuess']
+  const [pantallaActiva, setPantallaActiva] = useState('EnterNumber');
+  // const [gameStarted, setGameStarted] = useState(false);
+  const [modalStatus, setModalStatus] = useState(false);
+  const [mensajeModal, setMensajeModal] = useState('');
+
+  // useEffect(() => {
+  //   if( gameStarted === false ) setPantallaActiva('EnterNumber');
+  // }, [gameStarted])
+  
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {
+        (pantallaActiva === 'EnterNumber') && 
+          <EnterNumberScreen
+            modalStatus={ modalStatus }
+            setModalStatus={ setModalStatus }
+            mensajeModal={ mensajeModal }
+            setMensajeModal={ setMensajeModal }
+            setPantallaActiva={ setPantallaActiva }
+            />
+      }
+
+      {
+        (pantallaActiva === 'OpponentGuess') && <OpponentGuessScreen />
+      }
+
+      {
+        (pantallaActiva === 'GameOver') && <GameOverScreen />
+      }
+      
       <StatusBar style="auto" />
     </View>
   );
